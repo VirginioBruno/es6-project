fede442fa0c5bb4c0b2e01af90227ff178b5dfe7
Bruno Virginio <bruno.virginio23@gmail.com>
2019-10-02 00:03:02 -0300

    starting the repo project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-alert('Salvando')
+class App {
+    constructor(){
+        this.repositories =[];
+
+        this.formEl = document.querySelector('#repo-form');
+
+        this.registerHandlers()
+    }
+
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event){
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars8.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat.com.br'
+        });
+
+        console.log(this.repositories);
+    }
+}
+
+new App();
