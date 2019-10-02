55c2fe12fba84aafb80c705c36fae4fb53e2e969
Bruno Virginio <bruno.virginio23@gmail.com>
2019-10-01 22:42:09 -0300

    webpack added

--- bundle.js
+++ bundle.js
@@ -1,104 +1,113 @@
-//OBJECT SHORT SYNTAX
-// const nome = 'Bruno';
-// const idade = 23;
-// const usuario = {
-//     nome,
-//     idade,
-//     empresa: 'Teste',
-// };
-// console.log(usuario);
-//TEMPLATE LITERALS
-// const nome = 'Bruno';
-// const idade = 23;
-// console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
-//------------------------------------------------------------------------------------------------------------------------------------------
-//OPERADORES REST E SPREAD
-//REST
-// REST COM OBJETO
-// const usuario = {
-//     nome: 'Bruno',
-//     idade: '23',
-//     empresa: 'Teste'
-// };
-// const { nome, ...resto } = usuario;
-// console.log(nome);
-// console.log(resto);
-// REST COM OBJETO
-// const arr = [1,2,3,4];
-// const [ a, b, ...c ] = arr;
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// REST COM PARAMETROS NA FUNÇÃO
-// function soma(...params){
-//     return params.reduce((total, next) => total + next);
-// }
-// console.log(soma(1,2,3));
-//SPREAD
-//SPREAD COM ARRAY
-// const arr1 = [1,2,3];
-// const arr2 = [1,2,3];
-// const arr3 = [...arr1, ...arr2];
-//SPREAD COM OBJETOS
-// const usuario = {
-//     nome: 'Bruno',
-//     idade: 23,
-//     empresa: 'Teste'
-// }
-// const usuario2 = { ...usuario, nome: 'Teste Nome' };
-// console.log(usuario2);
-//------------------------------------------------------------------------------------------------------------------------------------------
-//DESESTRUTURAÇÃO
-// const usuario = {
-//     nome: 'Bruno',
-//     idade: 23,
-//     endereco: {
-//         cidade: 'São Bernando do Campo',
-//         estado: 'São Paulo'
-//     },
-// };
-// const { nome, idade, endereco: { cidade, estado } } = usuario;
-// function mostraNome({nome, idade}){
-//     console.log(nome);
-// }
-// console.log(nome)
-// console.log(idade)
-// console.log(cidade)
-// console.log(estado)
-//------------------------------------------------------------------------------------------------------------------------------------------
-//VALORES PADRÃO
-//const soma = (a = 2, b = 3) => a + b;
-//------------------------------------------------------------------------------------------------------------------------------------------
-//MANIPULANDO ARRAYS
-// const arr = [1,2,3,4,5,6]
-// const newArr = arr.map((item, index) => item + index);
-// const sum = arr.reduce((total, next) => total + next);
-// const filter = arr.filter(item => item % 2 === 0);
-// const find = arr.find(item => item === 4);
-// console.log(find);
-//-------------------------------------------------------------------------------------------------------------------------------------------
-//CRIANDO CLASSES COM EC6
-// class List{
-//     constructor(){
-//         this.data = [];
-//     }
-//     add(data){
-//         this.data.push(data);
-//         console.log(this.data);
-//     }
-// }
-// class TodoList extends List {
-//     constructor(){
-//         super();
-//         this.usuario = 'Bruno';
-//     }
-//     mostraUsuario(){
-//         console.log(this.usuario);
-//     }
-// }
-// const MinhaLista = new TodoList();
-// document.getElementById('novotodo').onclick = function(){
-//     MinhaLista.add('Novo todo');
-// }
-// MinhaLista.mostraUsuario();
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./funcoes.js":
+/*!********************!*\
+  !*** ./funcoes.js ***!
+  \********************/
+/*! exports provided: soma */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
 "use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"soma\", function() { return soma; });\nfunction soma(a, b) {\n  console.log(a + b);\n}\n\n//# sourceURL=webpack:///./funcoes.js?");
+
+/***/ }),
+
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _funcoes__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./funcoes */ \"./funcoes.js\");\n\nconsole.log(Object(_funcoes__WEBPACK_IMPORTED_MODULE_0__[\"soma\"])(1, 2));\n\n//# sourceURL=webpack:///./main.js?");
+
+/***/ })
+
+/******/ });
